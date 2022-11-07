89e3081c6111aca394894492e880b3352a421782
Beamtools <117571617+Beamtools@users.noreply.github.com>
2022-11-07 11:33:12 +0700

    Add files via upload

---
+++ installed.sh
@@ -0,0 +1,73 @@
+termux-setup-storage
+apt update && apt upgrade -y
+apt install curl -y
+apt install wget -y
+apt install python -y
+apt install python2 -y
+apt install cowsay -y
+apt install git -y
+apt install dart -y
+apt install toilet -y
+apt install fish -y
+apt install figlet -y
+apt install php -y
+apt install php -y
+apt install ruby -y
+apt install perl -y
+apt install clang -y
+apt install macchanger -y
+apt install jq -y
+apt install tor -y
+pkg install openssh -y
+apt install openssly
+apt-get update -y
+apt-get upgrade -y
+pip install --upgrade pip
+pip install requests
+pip install colorama
+pip install bs4
+pip install sqlmap
+pip2 install requests
+pip2 install colorama
+pip2 install bs4
+pip3 install requests
+pip3 install bs4 
+pip3 install colorama
+apt install nmap -y
+pip install lolcat
+git clone https://github.com/EH4404/freefc
+git clone https://github.com/palahsu/DDoS-Ripper
+git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
+git clone https://github.com/EH4404/ehstore
+git clone https://github.com/Sohil876/Termux-zsh
+apt install zsh -y
+apt install root-repo -y
+apt install tsu -y
+apt install x11-repo -y
+git clone https://github.com/remo7777/T-Header.git
+git clone https://github.com/sabri-zaki/EasY_HaCk
+git clone https://github.com/mishakorzik/AllHackingTools
+apt install toilet figlet exa wget ruby -y
+gem install lolcat 
+dpkg --configure -a
+apt update && apt upgrade -y
+apt update -y
+git clone https://github.com/jaykali/maskphish
+git clone https://github.com/may215/awesome-termux-hacking
+git clone https://github.com/noob-hackers/hacklock
+git clone https://github.com/noob-hackers/ipdrone
+apt install install git python3 php openssh -y
+git clone https://github.com/KasRoudra/PyPhisher
+pip3 install pyphisher
+git clone https://github.com/Gameye98/Lazymux
+pip install wordlist 
+git clone --depth=1 https://github.com/htr-tech/zphisher.git
+pkg install tur-repo -y
+pkg install proot -y
+gem install ngrok
+pkg install zip wget -y
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+unzip ngrok-stable-linux-arm.zip
+chmod +x ngrok
+clear
+
